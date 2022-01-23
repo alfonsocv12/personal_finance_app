77457d05fb669151a7fa0f55c7f20d076cadb90d
alfonsocv12 <alfonsocvu@gmail.com>
2022-01-22 18:17:42 -0700

    Good version of the navbar

--- lib/components/navBar.dart
+++ lib/components/navBar.dart
@@ -18,36 +18,53 @@ class NavBar {
     );
   }
 
+  Widget selectedIconElipse({required Icon icon}) {
+    return Container(
+      width: 60,
+      decoration: BoxDecoration(
+        color: Colors.blueGrey.shade300,
+        borderRadius: BorderRadius.circular(20)
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(8),
+        child: icon,
+      ),
+    );
+  }
+
   Widget getNavBar(cls, context, tabIndex) {
     _context = context;
 
-    return Container(
-      decoration: const BoxDecoration(
-        boxShadow:  [
-          BoxShadow(
-            color: Colors.grey,
-            blurRadius: 2
-          )
-        ]
-      ),
-      child: BottomNavigationBar(
-        unselectedItemColor: Colors.black,
-        items: const [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home_outlined),
-            label: 'Home',
-            activeIcon: Icon(Icons.home_filled)
+    return BottomNavigationBar(
+      backgroundColor: Colors.blueGrey.shade100,
+      unselectedItemColor: Colors.black,
+      selectedItemColor: Colors.black,
+      selectedFontSize: 14,
+      unselectedFontSize: 14,
+      currentIndex: tabIndex,
+      onTap: _onTab,
+      items: [
+        BottomNavigationBarItem(
+          icon: const Padding(
+            padding: EdgeInsets.all(8),
+            child: Icon(Icons.home_outlined)
           ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.settings_outlined),
-            label: 'Settings',
-            activeIcon: Icon(Icons.settings),
-          )
-        ],
-        currentIndex: tabIndex,
-        selectedItemColor: Colors.blueGrey.shade500,
-        onTap: _onTab,
-      ),
+          label: 'Home',
+          activeIcon: selectedIconElipse(
+            icon: const Icon(Icons.home_filled)
+          ),
+        ),
+        BottomNavigationBarItem(
+          icon: const Padding(
+            padding: EdgeInsets.all(8),
+            child: Icon(Icons.settings_outlined)
+          ),
+          label: 'Settings',
+          activeIcon: selectedIconElipse(
+            icon: const Icon(Icons.settings)
+          ),
+        )
+      ],
     );
   }
 }
